9eafb5a17a432ec3b888343eb3556afbd66221da
ThomasHatesIT <thomasbernardo910@gmail.com>
2026-01-12 02:27:15 +0800

    ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Vercel expects 'build' instead of 'dist'
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // backend server
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
